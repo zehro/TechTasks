5197ff9f94662b56a25ba192608ad0c108837df5
Andrew X <me@andrewxue.com>
2017-10-26 21:47:17 -0400

    -add task_demo scene for debugging task UI -also added TestMovement script for very very very basic movement

---
+++ Assets/Scripts/Task System/TestMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+
+    private static readonly IDictionary<KeyCode, Vector3> KEYCODE_TO_DIRECTION = new Dictionary<KeyCode, Vector3>() {
+        { KeyCode.W, Vector3.forward },
+        { KeyCode.D, Vector3.right },
+        { KeyCode.S, Vector3.back },
+        { KeyCode.A, Vector3.left }
+    };
+
+    [SerializeField]
+    private float speedMultiplier;
+
+    // Update is called once per frame
+    private void Update() {
+        DoMovement(GetDirection());
+    }
+
+    private IList<Vector3> GetDirection() {
+        List<Vector3> directions = new List<Vector3>();
+        foreach (KeyValuePair<KeyCode, Vector3> pair in KEYCODE_TO_DIRECTION) {
+            if (Input.GetKey(pair.Key)) {
+                directions.Add(pair.Value);
+            }
+        }
+        return directions;
+    }
+
+    private void DoMovement(IList<Vector3> directions) {
+        foreach (Vector3 direction in directions) {
+            transform.position += (direction * speedMultiplier);
+        }
+    }
+}
