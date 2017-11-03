c3aa7db83f20258e8f5eb5d033a428ed7a984449
Alex Rushing <arushing95@gmail.com>
2017-11-03 13:43:22 -0400

    Use different masks for menu instead of scenes

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -10,4 +10,34 @@ public class MenuManager : MonoBehaviour {
 		SceneManager.LoadScene (sceneName, LoadSceneMode.Single);
 	}
 
+	public void leaveMenu(string maskName)
+	{
+		GameObject oldMask = GameObject.Find("Mask");
+		Transform[] trans = GameObject.Find("Canvas").GetComponentsInChildren<Transform>(true);
+		print (oldMask.name);
+		print (maskName);
+		foreach (Transform t in trans) {
+			if (t.gameObject.name == maskName) {
+				print (t.gameObject.name);
+				oldMask.SetActive (false);
+				t.gameObject.SetActive (true);
+			}
+		}
+	}
+
+	public void backToMenu(string maskName)
+	{
+		GameObject oldMask = GameObject.Find(maskName);
+		Transform[] trans = GameObject.Find("Canvas").GetComponentsInChildren<Transform>(true);
+		print (oldMask.name);
+		print (maskName);
+		foreach (Transform t in trans) {
+			if (t.gameObject.name == "Mask") {
+				print (t.gameObject.name);
+				oldMask.SetActive (false);
+				t.gameObject.SetActive (true);
+			}
+		}
+	}
+
 }
