7b1a8cd9a9a353995e15fa9659a48969e20fc2df
zehro <wisdom0music@gmail.com>
2017-11-06 22:16:26 -0500

    Pushing in the nice controller. Still neds work

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float speed;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, speed * Time.deltaTime);
+    }
+}
