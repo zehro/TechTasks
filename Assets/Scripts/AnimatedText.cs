211340ebf1e33f288225671ed3cbf3753d1f08dd
Alex Rushing <arushing95@gmail.com>
2017-11-09 17:26:31 -0500

    Add chalk script and font

---
+++ Assets/Scripts/AnimatedText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedText : MonoBehaviour {
+
+	public float letterPaused = 0.01f;
+	private string message;
+	public Text textComp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		textComp = GetComponent<Text> ();
+		message = textComp.text;
+		textComp.text = "";
+		StartCoroutine (TypeText ());
+	}
+
+	IEnumerator TypeText()
+	{
+		foreach (char letter in message.ToCharArray()) 
+		{
+			textComp.text += letter;
+			yield return 0;
+			yield return new WaitForSeconds(letterPaused);
+		}
+	}
+
+}
