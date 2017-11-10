a7a5bbffa452e59a616546ded6c62b3c3eac0ad5
Andrew X <me@andrewxue.com>
2017-11-09 20:33:03 -0500

    main menu should no longer be missing scripts.

--- Assets/Scripts/AnimatedText.cs
+++ Assets/Scripts/AnimatedText.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AnimatedText : MonoBehaviour {
+    public float letterPaused = 0.01f;
+    private string message;
+    public Text textComp;
 
-	public float letterPaused = 0.01f;
-	private string message;
-	public Text textComp;
+    // Use this for initialization
+    private void Start() {
+        textComp = GetComponent<Text>();
+        message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+    }
 
-	// Use this for initialization
-	void Start ()
-	{
-		textComp = GetComponent<Text> ();
-		message = textComp.text;
-		textComp.text = "";
-		StartCoroutine (TypeText ());
-	}
-
-	IEnumerator TypeText()
-	{
-		foreach (char letter in message.ToCharArray()) 
-		{
-			textComp.text += letter;
-			yield return 0;
-			yield return new WaitForSeconds(letterPaused);
-		}
-	}
-
+    private IEnumerator TypeText() {
+        foreach (char letter in message.ToCharArray()) {
+            textComp.text += letter;
+            yield return new WaitForSeconds(letterPaused);
+        }
+    }
 }
