d4f740894c82c607249c10237175996fb3fa2eb2
Nikki Nikolenko <nikki.nikolenko@gmail.com>
2017-11-13 20:44:59 -0500

    Adding Nikki's AI Code. Needs to be converted

---
+++ Assets/Scripts/AI/ClickToMove.cs
@@ -0,0 +1,137 @@
+﻿// ClickToMove.cs
+using System;
+using UnityEngine;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+	RaycastHit hitInfo = new RaycastHit();
+	UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+    public GameObject wayPoint;
+    public GameObject wayPoint1;
+    public GameObject wayPoint2;
+    public GameObject movingWaypoint;
+    public float futureTLimit = 1.5f;
+    //private MovingCubeScript movingCubeScript;
+    private LocomotionSimpleAgent locomotion;
+    public GameObject[] wayPointList;
+    private String patrolState = "wayPoint1";
+    private int currWaypointIndex = -1;
+    public enum Behavior
+    {
+        ClickToMove, 
+        Patrol,
+        Random,
+        MovingTarget,
+        Idle,
+        PickARandomWaypoint
+    }
+    public Behavior behavior = Behavior.Patrol;
+
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+        locomotion = GetComponent<LocomotionSimpleAgent>();
+        anim = GetComponent<Animator>();
+        //movingCubeScript = movingWaypoint.GetComponent<MovingCubeScript>();
+
+    }
+	void Update () {
+
+        if (behavior == Behavior.ClickToMove)
+        {
+            locomotion.shouldWalk(false);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                    agent.destination = hitInfo.point;
+            }
+
+        } else if (behavior == Behavior.Patrol)
+        {
+            locomotion.shouldWalk(true);
+            if (patrolState == "wayPoint1")
+            {
+                print("wayPoint1");
+                locomotion.setWayPoint(wayPoint1.transform.position);
+                patrolState = "waiting to complete 1";
+                //print(Vector3.Magnitude(transform.position - wayPoint2.transform.position));
+
+            } else if (patrolState == "waiting to complete 1")
+            {
+                if (locomotion.isComplete(wayPoint1.transform.position))
+                {
+                    print("wayPoint2");
+                    locomotion.setWayPoint(wayPoint2.transform.position);
+                    patrolState = "waiting to complete 2";
+
+                }
+                
+
+            } else if (patrolState == "waiting to complete 2")
+            {
+                if (locomotion.isComplete(wayPoint2.transform.position))
+                {
+                    print("wayPoint1 (new state)");
+                    locomotion.setWayPoint(wayPoint1.transform.position);
+                    patrolState = "waiting to complete 1";
+
+                }
+
+            }
+            
+            
+
+        } else if (behavior == Behavior.MovingTarget)
+        {
+            //anim.SetBool("walk", false);
+            locomotion.shouldWalk(false);
+
+            if (!locomotion.isComplete(movingWaypoint.transform.position))
+            {
+                
+                //dist between moving object and NPC 
+                float dist = (movingWaypoint.transform.position - transform.position).magnitude;
+
+                //a super simple mapping of dist to time in sec:
+                //feel free to come up with something better
+                //you can also use the equations from the Gamasutra projectile article
+                //detailed in the assignment PDF for a more accurate method
+                float futureT = 0.1f * dist;
+                //print(futureT);
+
+                futureT = Mathf.Min(futureT, futureTLimit); //limit on how far ahead to look
+                                                            //timeBetweenPredictions = futureT * 900f;
+                                                            //extrapolate assuming constant Vel and the futureT intercept estimate
+                //Vector3 futureMoverPos = movingWaypoint.transform.position + new Vector3(movingCubeScript.SpeedX, 0, movingCubeScript.SpeedZ) * futureT;
+
+                //update the target waypoint
+                //locomotion.setWayPoint(futureMoverPos);
+  
+
+            } else
+            {
+                behavior = Behavior.Idle;
+            }
+
+
+        } else if (behavior == Behavior.PickARandomWaypoint)
+        {
+            if (currWaypointIndex == -1 || locomotion.isComplete(wayPointList[currWaypointIndex].transform.position))
+            {
+                int waypointIndex = UnityEngine.Random.Range(0, wayPointList.Length);
+                while (waypointIndex == currWaypointIndex)
+                {
+                    waypointIndex = UnityEngine.Random.Range(0, wayPointList.Length);
+
+                }
+                currWaypointIndex = waypointIndex;
+                locomotion.setWayPoint(wayPointList[currWaypointIndex].transform.position);
+
+            }
+            
+        } 
+
+
+    }
+}
---
+++ Assets/Scripts/AI/LocomotionSimpleAgent.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+[RequireComponent (typeof (Animator))]
+public class LocomotionSimpleAgent : MonoBehaviour {
+	Animator anim;
+	UnityEngine.AI.NavMeshAgent agent;
+	Vector2 smoothDeltaPosition = Vector2.zero;
+	Vector2 velocity = Vector2.zero;
+    public bool shouldMove;
+    public bool walk;
+    public float isCompleteDistance = 1.5f;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		agent.updatePosition = false;
+        anim.SetBool("walk", walk);
+    }
+    public void setWayPoint(Vector3 point)
+    {
+        agent.destination = point;
+
+    }
+
+    public void shouldWalk(bool shouldWalk)
+    {
+        walk = shouldWalk;
+        anim.SetBool("walk", shouldWalk);
+    }
+    public bool isComplete(Vector3 pos)
+    {
+        return Vector3.Magnitude(transform.position - pos) < 1.5f;
+
+    }
+
+    void Update () {
+        anim.SetBool("walk", walk);
+        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+
+		// Map 'worldDeltaPosition' to local space
+		float dx = Vector3.Dot (transform.right, worldDeltaPosition);
+		float dy = Vector3.Dot (transform.forward, worldDeltaPosition);
+		Vector2 deltaPosition = new Vector2 (dx, dy);
+
+		// Low-pass filter the deltaMove
+		float smooth = Mathf.Min(1.0f, Time.deltaTime/0.5f);
+		smoothDeltaPosition = Vector2.Lerp (smoothDeltaPosition, deltaPosition, smooth);
+
+		// Update velocity if delta time is safe
+		if (Time.deltaTime > 1e-5f)
+			velocity = smoothDeltaPosition / Time.deltaTime;
+
+		shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+        velocity.x = Mathf.Clamp(velocity.x, -0.5f, 0.5f);
+        velocity.y = Mathf.Clamp(velocity.y, 0f, 2.7f);
+        velocity.x = Mathf.Lerp(velocity.x, agent.velocity.x,5f * Time.deltaTime);
+        velocity.y = Mathf.Lerp(velocity.y, agent.velocity.y, 5 * Time.deltaTime);
+
+
+        // Update animation parameters
+        
+        anim.SetBool("move", shouldMove);
+        anim.SetFloat("velx", velocity.x);
+
+       
+        anim.SetFloat("vely", velocity.y);
+        
+
+		LookAt lookAt = GetComponent<LookAt> ();
+		if (lookAt)
+			lookAt.lookAtTargetPosition = agent.steeringTarget + transform.forward;
+
+
+        //		// Pull character towards agent
+       // if (worldDeltaPosition.magnitude > agent.radius)
+        //	transform.position = agent.nextPosition - 0.9f*worldDeltaPosition;
+
+        //		// Pull agent towards character
+        if (worldDeltaPosition.magnitude > agent.radius)
+			agent.nextPosition = transform.position + 0.9f*worldDeltaPosition;
+	}
+
+	void OnAnimatorMove () {
+        Quaternion rotation = transform.rotation;
+        // Update postion to agent position
+        //		transform.position = agent.nextPosition;
+
+        // Update position based on animation movement using navigation surface height
+        Vector3 position = anim.rootPosition;
+		position.y = agent.nextPosition.y;
+        transform.position = position;
+		
+	}
+}
