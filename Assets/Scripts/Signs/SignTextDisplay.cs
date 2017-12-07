1f6f607fc08f6f278b6645a438eee356c1d78bbb
Andrew X <me@andrewxue.com>
2017-12-07 02:46:37 -0500

    Added signage scripts. Pause button is Fire3. Read sign is Fire2.

---
+++ Assets/Scripts/Signs/SignTextDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignTextDisplay : MonoBehaviour {
+
+    [SerializeField]
+    private Text signText;
+
+    public void ShowSignDisplay(string message) {
+        signText.text = message;
+        gameObject.SetActive(true);
+    }
+
+    public void HideSignDisplay() {
+        gameObject.SetActive(false);
+    }
+}
