b5b1681c92959097a45c655fa5b96e219395eee4
Andrew X <me@andrewxue.com>
2017-12-07 06:07:47 -0500

    getting all small stars unlocks a big star that ends the game.

--- Assets/Scripts/Star System/StarManager.cs
+++ Assets/Scripts/Star System/StarManager.cs
@@ -18,22 +18,38 @@ public class StarManager : MonoBehaviour {
 
     private IDictionary<int, Star> dict;
 
+    private CollectibleStar bigStar;
+
     private void Start() {
         this.dict = new Dictionary<int, Star>();
 
         foreach (CollectibleStar star in FindObjectsOfType<CollectibleStar>()) {
-            Star uiStar = Instantiate<Star>(uiPrefab);
-            dict.Add(star.gameObject.GetInstanceID(), uiStar);
-            uiStar.transform.SetParent(starHolder.transform);
+            if (!star.IsBigStar) {
+                Star uiStar = Instantiate<Star>(uiPrefab);
+                dict.Add(star.gameObject.GetInstanceID(), uiStar);
+                uiStar.transform.SetParent(starHolder.transform);
+            } else {
+                if (bigStar != null) {
+                    throw new UnityException("Only one big star is allowed!");
+                }
+                this.bigStar = star;
+            }
+        }
+        if (bigStar == null) {
+            throw new UnityException("No big star in scene!");
         }
     }
 
     public void CompleteStar(int instanceId) {
+        if (bigStar.gameObject.GetInstanceID() == instanceId) {
+            pause.DoGameOver(this.timer.GetTimeFormatted());
+            return;
+        }
         dict[instanceId].SetColor(false);
         dict.Remove(instanceId);
 
         if (dict.Count == 0) {
-            pause.DoGameOver(timer.GetTimeFormatted());
+            bigStar.ActivateStar();
         }
     }
 }
