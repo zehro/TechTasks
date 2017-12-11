9071cff52b8af6dce5f1a0b51f65ccc41ae2b7f4
Andrew X <me@andrewxue.com>
2017-12-11 00:27:29 -0500

    fix issue where BombStateMachine.cs was spamming new game objects

--- Assets/Scripts/AI/BombStateMachine.cs
+++ Assets/Scripts/AI/BombStateMachine.cs
@@ -9,16 +9,19 @@ public class BombStateMachine : MonoBehaviour {
     private UnityEngine.AI.NavMeshAgent agent;
     private Animator anim;
     public GameObject player;
-	private Animator playerAnimator;
+    private Animator playerAnimator;
     public float futureTLimit = 1.5f;
+
+    private GameObject lookahead;
 
     //private MovingCubeScript movingCubeScript;
     private AIMovement locomotion;
 
     public GameObject[] wayPointList;
     private int currWaypointIndex = -1;
-	private Boolean dead = false;
-	private Boolean isFalling = false;
+    private Boolean dead = false;
+    private Boolean isFalling = false;
+
     public enum Behavior {
         Patrol,
         Chase
@@ -30,50 +33,50 @@ public class BombStateMachine : MonoBehaviour {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         locomotion = GetComponent<AIMovement>();
         anim = GetComponent<Animator>();
-		playerAnimator = player.GetComponent<Animator> ();
+        playerAnimator = player.GetComponent<Animator>();
         //movingCubeScript = movingWaypoint.GetComponent<MovingCubeScript>();
     }
 
     private void Update() {
-		if (playerAnimator.GetBool ("isFalling")) {
-			jumpOn ();
-			print ("fall");
-		} else if (Vector3.Distance(player.transform.position, this.transform.position) < 1) {
-			print ("bomb");
-			StartCoroutine (waitAndBomb());
-			StartCoroutine (bomb());
-		}
+        if (playerAnimator.GetBool("isFalling")) {
+            jumpOn();
+            print("fall");
+        } else if (Vector3.Distance(player.transform.position, this.transform.position) < 1) {
+            print("bomb");
+            StartCoroutine(waitAndBomb());
+            StartCoroutine(bomb());
+        }
         transition();
     }
 
-	IEnumerator waitAndBomb()
-	{
-		transform.localScale += new Vector3(.03F,.03f, .03f);
-		yield return new WaitForSeconds(1f);
-//		Destroy (this.gameObject);
-	}
-	IEnumerator bomb() {
-		yield return new WaitForSeconds(2f);
-		Destroy (this.gameObject);
-	}
+    private IEnumerator waitAndBomb() {
+        transform.localScale += new Vector3(.03F, .03f, .03f);
+        yield return new WaitForSeconds(1f);
+        //		Destroy (this.gameObject);
+    }
+
+    private IEnumerator bomb() {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+
     private void transitionToStatePatrol() {
-                print ("patrol");
-		if (currWaypointIndex == -1 || locomotion.reachedWaypoint(wayPointList[currWaypointIndex].transform)) {
+        print("patrol");
+        if (currWaypointIndex == -1 || locomotion.reachedWaypoint(wayPointList[currWaypointIndex].transform)) {
             int waypointIndex = UnityEngine.Random.Range(0, wayPointList.Length);
             while (waypointIndex == currWaypointIndex) {
                 waypointIndex = UnityEngine.Random.Range(0, wayPointList.Length);
             }
             currWaypointIndex = waypointIndex;
             print("patrol waypoint " + currWaypointIndex);
-			locomotion.setWayPoint(wayPointList [currWaypointIndex].transform);
+            locomotion.setWayPoint(wayPointList[currWaypointIndex].transform);
         }
     }
 
     private void transitionToStateChase() {
-		
         //anim.SetBool("walk", false);
-//        locomotion.shouldWalk(false);
-		locomotion.setWayPoint(player.transform);
+        //        locomotion.shouldWalk(false);
+        locomotion.setWayPoint(player.transform);
         //        if (!locomotion.isComplete(player.transform.position))
         //        {
         print("chase");
@@ -82,10 +85,17 @@ public class BombStateMachine : MonoBehaviour {
         futureT = Mathf.Min(futureT, 1000); //limit on how far ahead to look
                                             //extrapolate assuming constant Vel and the futureT intercept estimate
         Vector3 futureMoverPos = player.transform.position + player.GetComponent<Rigidbody>().velocity * futureT;
-		var newTrans = new GameObject().transform;
-		newTrans.position = futureMoverPos;
+
+        GameObject go;
+        if (lookahead == null) {
+            go = new GameObject();
+        } else {
+            go = lookahead;
+        }
+        var newTrans = go.transform;
+        newTrans.position = futureMoverPos;
         //update the target waypoint
-		locomotion.setWayPoint(newTrans);
+        locomotion.setWayPoint(newTrans);
         //        }
     }
 
@@ -109,44 +119,44 @@ public class BombStateMachine : MonoBehaviour {
             }
         }
     }
-	private bool jumpOn() {
-		
-		float dis = Vector3.Distance(player.transform.position, this.transform.position);
-		//		print ("dis: " + dis);
 
-		if (dis< 2) {
-			StartCoroutine(waitAndDie());
-			return true;
-		}
-		return false;
-	}
+    private bool jumpOn() {
+        float dis = Vector3.Distance(player.transform.position, this.transform.position);
+        //		print ("dis: " + dis);
 
-	IEnumerator waitAndDie()
-	{
-		yield return new WaitForSeconds(1f);
-		Destroy (this.gameObject);
-	}
+        if (dis < 2) {
+            StartCoroutine(waitAndDie());
+            return true;
+        }
+        return false;
+    }
+
+    private IEnumerator waitAndDie() {
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+
     private bool inRange() {
         Vector3 direction = player.transform.position - this.transform.position;
         float angle = Vector3.Angle(direction, this.transform.forward);
-		float dis = Vector3.Distance(player.transform.position, this.transform.position);
-//		print ("dis: " + dis);
+        float dis = Vector3.Distance(player.transform.position, this.transform.position);
+        //		print ("dis: " + dis);
 
-		if (dis <10 || (dis < 15 && angle < 30)) {
-			return true;
+        if (dis < 10 || (dis < 15 && angle < 30)) {
+            return true;
         }
         return false;
         //        return true;
     }
-//	void OnCollisionEnter(Collision collision) {
-//		if (collision.gameObject.CompareTag ("Player")) {
-//			
-//			Debug.Log ("COLLIDE");
-//			if (playerAnimator.GetBool ("isFalling")) {
-//				Debug.Log ("fall");
-//				Destroy (this.gameObject);
-//			}
-//		}
-//	}
 
+    //	void OnCollisionEnter(Collision collision) {
+    //		if (collision.gameObject.CompareTag ("Player")) {
+    //
+    //			Debug.Log ("COLLIDE");
+    //			if (playerAnimator.GetBool ("isFalling")) {
+    //				Debug.Log ("fall");
+    //				Destroy (this.gameObject);
+    //			}
+    //		}
+    //	}
 }
